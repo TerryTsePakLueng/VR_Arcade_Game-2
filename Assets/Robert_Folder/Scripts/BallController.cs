f84aeb21f7f23bba756bbe3f5d1780b702764325
Robert Day <rday8@my.centennialcollege.ca>
2021-02-24 19:37:16 -0500

    Robs scene
    pushing my work

---
+++ Assets/Robert_Folder/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public SkiBallManager skiBallMgr;
+    private void Awake()
+    {
+        skiBallMgr = FindObjectOfType<SkiBallManager>();
+    }
+
+    IEnumerator ResetBall()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+        skiBallMgr.RespawnBall();
+    }
+    public void SpawnNewBall()
+    {
+        StartCoroutine(ResetBall());
+    }
+
+}
---
+++ Assets/Robert_Folder/Scripts/SkiBallManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkiBallManager : MonoBehaviour
+{
+    public GameObject ballPreFab;
+    public int ballsThrown;
+
+    public void RespawnBall()
+    {
+        Instantiate(ballPreFab);
+    }
+}
