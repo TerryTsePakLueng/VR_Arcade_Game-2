ba8444ca4b80de12249be1a934cf3b7082adf7ad
Robert Day <rday8@my.centennialcollege.ca>
2021-03-23 16:09:47 -0400

    Gun shoots
    the gun for the balloon game now shoots in debug

--- Assets/Robert_Folder/Scripts/Contollers/GunController.cs
+++ Assets/Robert_Folder/Scripts/Contollers/GunController.cs
@@ -2,26 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VR;
+using Valve.VR;
 
 public class GunController : MonoBehaviour
 {
-    public Transform nozzle;
-    public Camera cam;
-    public float fireRate;
-    public float fireRange;
-    float nextFire;
+    public BalloonGameManager balloonGameMgr;
 
+    public bool isHoldingGun = false;
     private void Start()
     {
+        SteamVR_Actions.default_GrabPinch.AddOnStateDownListener(TriggerPressed, SteamVR_Input_Sources.Any);
+        balloonGameMgr = FindObjectOfType<BalloonGameManager>();
         
     }
-    private void Update()
+
+    private void TriggerPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        
+        if (!isHoldingGun && balloonGameMgr.isPlayingBalloonGame)
+        {
+            balloonGameMgr.shotsLeftToTake = balloonGameMgr.totalShotsToTake;
+            isHoldingGun = true;
+        }
+        if (isHoldingGun && balloonGameMgr.isPlayingBalloonGame)
+        {
+            ShootGun();
+        Debug.Log("Gun has been shot");
+        }
     }
 
     public void ShootGun()
     {
-
     }
 }
