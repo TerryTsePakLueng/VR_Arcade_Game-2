9ea941fb89f7d4c767663dbd310bc22f601862a1
TerryTsePakLueng <terrytse22@gmail.com>
2021-03-24 15:59:57 -0400

    Watch animation & UI
    UI done

---
+++ Assets/Terry_Folder/Script/WatchManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WatchManager : MonoBehaviour
+{
+    public Animator watchAnimator;
+    public GameObject watchMainMenuCanvas;
+    public GameObject watchMyTicketsCanvas;
+    public bool isOpen = false;
+
+    public void Start()
+    {
+
+    }
+
+    public void OnButtonPress()
+    {
+        if (!isOpen)
+        {
+            watchMyTicketsCanvas.SetActive(false);
+            watchMainMenuCanvas.SetActive(true);
+            watchAnimator.SetBool("isOpen", true);
+            isOpen = true;
+        }
+        else
+        {
+            watchAnimator.SetBool("isOpen", false);
+            isOpen = false;
+        }
+    }
+
+    public void OnMyTicketsButtonPress()
+    {
+        watchMainMenuCanvas.SetActive(false);
+        watchAnimator.SetBool("isOpen", false);
+        DelayScreenAppearingForMyTickets();
+    }
+
+    public void OnBackButtonPress()
+    {
+        watchMyTicketsCanvas.SetActive(false);
+        watchAnimator.SetBool("isOpen", false);
+        DelayScreenAppearingForBack();
+    }
+
+    IEnumerator DelayScreenAppearingForMyTickets()
+    {
+        yield return new WaitForSeconds(1.1f);
+        watchMyTicketsCanvas.SetActive(true);
+        watchAnimator.SetBool("isOpen", true);
+    }
+
+    IEnumerator DelayScreenAppearingForBack()
+    {
+        yield return new WaitForSeconds(1.1f);
+        watchMainMenuCanvas.SetActive(true);
+        watchAnimator.SetBool("isOpen", true);
+    }
+}
