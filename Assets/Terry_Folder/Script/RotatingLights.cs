d81e7519c5c9577a3c4a69c62e196208fc6c9a6a
TerryTsePakLueng <terrytse22@gmail.com>
2021-03-27 16:26:07 -0400

    Radio and SpotLights
    added radio and button, spolights (rotating and flashing) made overall everything prettier. Removed FBX from hierarchy and replaced with prefabs.

---
+++ Assets/Terry_Folder/Script/RotatingLights.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingLights : MonoBehaviour
+{
+    public GameObject spotLight;
+    public float delay = 0.5f;
+    public bool isLooping = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MakeSpotLightBlink(delay));
+    }
+
+    IEnumerator MakeSpotLightBlink(float timeDelay)
+    {
+        while(isLooping == true)
+        {
+            spotLight.SetActive(false);
+            yield return new WaitForSeconds(timeDelay);
+            spotLight.SetActive(true);
+        }
+    }
+}
