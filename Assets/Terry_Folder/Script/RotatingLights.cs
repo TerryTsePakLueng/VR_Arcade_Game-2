32f21230e32176edbd089bbda81c4d9b180527c6
Robert Day <rday8@my.centennialcollege.ca>
2021-03-31 17:48:25 -0400

    Revert "TradingDisplay"
    This reverts commit ad5fae0d79f65904beb83d5d244534315064dcd4.

--- Assets/Terry_Folder/Script/RotatingLights.cs
+++ Assets/Terry_Folder/Script/RotatingLights.cs
@@ -4,58 +4,20 @@ using UnityEngine;
 
 public class RotatingLights : MonoBehaviour
 {
-    public float totalSeconds;     // The total of seconds the flash wil last
-    public float maxIntensity;     // The maximum intensity the flash will reach
-    public Light myLight;        // Your light
+    public GameObject spotLight;
+    public float delay = 0.5f;
+    public bool isLooping = true;
 
     private void Start()
     {
-        StartCoroutine(flashNow());
+        StartCoroutine(MakeSpotLightBlink(delay));
     }
 
-    public IEnumerator flashNow()
+    IEnumerator MakeSpotLightBlink(float delayTime)
     {
-        float waitTime = totalSeconds / 2;
-        // Get half of the seconds (One half to get brighter and one to get darker)
-        while (myLight.intensity > 0)
-        {
-            myLight.intensity -= Time.deltaTime / waitTime;        //Decrease intensity
-            yield return null;
-        }
-        while (myLight.intensity < maxIntensity)
-        {
-            myLight.intensity += Time.deltaTime / waitTime;        // Increase intensity
-            yield return null;
-        }
-        yield return null;
+        spotLight.SetActive(false);
+        yield return new WaitForSeconds(delayTime);
+        spotLight.SetActive(true);
     }
 
-
-    //public GameObject spotLight;
-    //public float delay;
-    //bool isOn;
-
-    //public void MakeBlink()
-    //{
-    //    spotLight.SetActive(false);
-    //    StartCoroutine(MakeSpotLightBlink(delay));
-    //}
-
-    //IEnumerator MakeSpotLightBlink(float delayTime)
-    //{
-    //    yield return new WaitForSeconds(delayTime);
-    //    spotLight.SetActive(true);
-    //}
-
-    //private void Update()
-    //{
-    //    if(!isOn)
-    //    {
-
-    //    }
-    //    else
-    //    {
-
-    //    }
-    //}
 }
