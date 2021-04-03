fb75184bb2bf1f27d253eeb1598626205b22ca7f
TerryTsePakLueng <terrytse22@gmail.com>
2021-04-03 16:17:47 -0400

    Added Hand Sensor and Trade Manager Script
    trade manager script made and hand sensor implemented.

---
+++ Assets/Terry_Folder/Script/HandScannerTriggerArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandScannerTriggerArea : MonoBehaviour
+{
+    TradeManager tradeMgr;
+
+    private void Awake()
+    {
+        tradeMgr = FindObjectOfType<TradeManager>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if(tradeMgr.scrollingAnimator.GetBool("isScrolled") == false)
+            {
+                tradeMgr.UpdateTotalPoints();
+                tradeMgr.ScrollDownTradingDisplay();
+            }
+            else if(tradeMgr.scrollingAnimator.GetBool("isScrolled") == true)
+            {
+                tradeMgr.ScrollUpTradingDisplay();
+            }
+        }
+    }
+}
